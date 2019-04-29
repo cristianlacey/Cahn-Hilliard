7c355c874af30702c267c4dbaeebbe45f66919be
Cristian Lacey <clacey@princeton.edu>
2019-04-29 16:56:14 -0400

    Added FD implementation with 2nd order centered diff in space, 1st ordered forward euler in time

---
+++ FD/2CDS_1FDT.py
@@ -0,0 +1,79 @@
+""" 2CDS_1FDT.py
+This file is a part of Cahn-Hilliard
+Authors: Cristian Lacey, Sijie Tong
+This file contains a routine to solve the Cahn-Hilliard equation using second
+order centered difference in space and first order forward Euler in time.
+"""
+import numpy as np
+import matplotlib.pyplot as plt
+
+from scipy import sparse
+from scipy import linalg
+
+# -------------------------------
+# FUNCTION DEFINITIONS
+# -------------------------------
+def update(phi,dt,lap):
+    '''
+    Updates current state of phi using second order centered difference
+    in space and first order forward Euler in time.
+        Args:
+            phi (np.array): Array in col major format of phase concentration.
+            dt (float): Timestep size
+            lap (sparse.dia_matrix): Laplacian operator with periodic BCs.
+        Returns:
+            phi_n (np.array): Array in col major format of phase concentration
+            in next timestep.
+    '''
+    # Calculate right-hand side of PDE
+    rhs = lap.dot(np.power(phi,3) - phi - (lap.dot(phi)))
+
+    # Step forward in time with first order Euler
+    phi_n = phi + dt*rhs
+
+    return phi_n
+
+# -------------------------------
+# INPUT PARAMETERS
+# -------------------------------
+N = 100 # lattice points per axis
+dx = 1 # lattice spacing
+dt = 0.01 # timestep size
+tsteps = 10001 # number of timesteps
+
+dump = 1000 # dump an image every 'dump' steps
+phi_avg = 0 # initial mean value of phi
+noise = 0.1 # initial amplitude of fluctuations
+
+# -------------------------------
+# INITIALIZATION
+# -------------------------------
+# Initialize phi as NxN matrix with random values bounded by phi_avg + noise/2
+phi = phi_avg*(np.ones((N,N))) + noise*(np.random.rand(N,N)-0.5)
+t = np.arange(0, tsteps*dt, dt)
+
+# Unravel phi in col major form
+phi = np.ravel(phi, order='F')
+
+# Define Laplace operator with periodic BCs, then convert to sparse.dia_matrix
+# object to leverage faster matrix multiplication of block-banded Laplacian
+A = sparse.diags([1,1,-2,1,1], [-(N-1),-1,0,1,(N-1)], shape=(N,N)).toarray()
+I = np.eye(N)
+lap = np.kron(I,A) + np.kron(A,I)
+lap = sparse.dia_matrix(lap)
+# plt.matshow(lap)
+# plt.show()
+
+# -------------------------------
+# MAIN LOOP
+# -------------------------------
+for i in range(np.size(t)):
+    print(t[i])
+    if i % dump == 0:
+        phi = phi.reshape((N,N),order='F')
+        plt.imshow(phi)
+        plt.colorbar()
+        plt.savefig('./t'+str(int(i/dump)).zfill(3)+'.png', dpi=300)
+        plt.clf()
+        phi = np.ravel(phi, order='F')
+    phi = update(phi,dt,lap)
